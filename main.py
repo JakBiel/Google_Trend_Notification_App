6afe873fca5772d42b77dd29163146dcdc5231f8
JakBiel <jakbiel1@gmail.com>
2024-06-19 14:58:09 +0200

    Testing pre-commit on all files in repo7

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from slack_sdk.errors import SlackApiError
 
 def fetch_trending_searches():
     # Creating a pytrends instance
-    pytrends = TrendReq(hl='en', tz=360)  # Setting the language to English and time zone (e.g., UTC+1 for Poland)
+    pytrends = TrendReq(
+        hl="en", tz=360
+    )  # Setting the language to English and time zone (e.g., UTC+1 for Poland)
 
     # Fetching the most popular searches from the last 24 hours
     trending_list = pytrends.trending_searches()
@@ -22,14 +24,14 @@ def fetch_trending_searches():
 
 
 def gemini_querying(trends):
-    my_api_key = os.getenv('GEMINI_TOKEN')
+    my_api_key = os.getenv("GEMINI_TOKEN")
 
     genai.configure(api_key=my_api_key)
     # The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel("gemini-1.5-flash")
 
-    response = model.generate_content(f"""I attached below a list of trends. Please make each trend's name started and ended with "*" sign.
-                                    Please attach to a trend in each row a proper category [define it short (max 3 words)]
+    response = model.generate_content(
+        f"""I attached below a list of trends. Please make each trend's name started and ended with "*" sign. Please attach to a trend in each row a proper category [define it short (max 3 words)]
                                     Dont't add any additional explainations to the list. Also, don't forget
                                     about showing the index of each element on the list. What's the last, please
                                     add related emoji at the end of each trend's row (which means not
@@ -38,39 +40,34 @@ def gemini_querying(trends):
                                     1.  *Fort Lauderdale state of emergency*  -  News :rotating_light:
                                     2.  *Boston Celtics*  -  Sports :basketball:
                                     3.  *The Boys Season 4*  -  TV :male_superhero:
-                                    {trends}""")
+                                    {trends}"""
+    )
     print(response.text)
 
     return response.text
 
 
 def send_slack_notification(message):
-    token = os.getenv('MY_SLACK_TOKEN')
+    token = os.getenv("MY_SLACK_TOKEN")
     channel = "#jakub_channel"
 
     client = WebClient(token=token)
     try:
-        response = client.chat_postMessage(
+        client.chat_postMessage(
             channel=channel,
             text="This is a fallback text with *bold* text",
-            blocks=[
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": message
-                    }
-                }
-            ]
+            blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": message}}],
         )
         print("Message sent successfully")
     except SlackApiError as e:
         print(f"Failed to send message: {e.response['error']}")
 
+
 @functions_framework.http
 def main(request):
     trends = fetch_trending_searches()
     results = gemini_querying(trends)
-    send_slack_notification(f"THIS is generated, analyzed and modified by Gemini:\n\n Most popular trends in Google browser in last 24 hours: \n {results}")
+    send_slack_notification(
+        f"THIS is generated, analyzed and modified by Gemini:\n\n Most popular trends in Google browser in last 24 hours: \n {results}"
+    )
     return "Success", 200
-
